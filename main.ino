f23559d23f441249ed8ca3c0c81e7081690105f6
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-10-03 14:56:48 +0000

    add main.ino

---
+++ main.ino
@@ -0,0 +1,39 @@
+// Created by: Bonnie Zhu
+// Created on: Oct 2023
+// Turns as RGB LED 
+
+void setup()
+
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
